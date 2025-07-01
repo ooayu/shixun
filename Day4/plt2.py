b5ce78cae95223d7c6a5682a0326b126b4f8d949
heyanru <he623069797@163.com>
2025-07-01 10:26:55 +0800

    third

--- Day4/plt2.py
+++ Day4/plt2.py
@@ -45,6 +45,6 @@ plt.show()
 plt.figure(figsize=(8, 8))
 plt.pie(gdp_2015, labels=cities, autopct='%1.1f%%', startangle=140)
 plt.title('2015年各个地区生产总值占比')
+#保存图片 在show之前
 plt.savefig('chengshi.png')
 plt.show()
-
